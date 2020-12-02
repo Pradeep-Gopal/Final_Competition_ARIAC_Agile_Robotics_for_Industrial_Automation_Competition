bf3183601e9499292fd71d8dbc75cd23c3bf3c9a
Govind <govindajithkumar@gmail.com>
2020-12-02 09:11:57 -0500

    DOXYGEN COMPLETED : utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,7 +1,12 @@
 #include "utils.h"
 
+/**
+ * Hash-map to get different model heights for different parts.
+ * They were modified because each part sinks into the surface a bit,
+ * in varying amounts.
+ */
 std::unordered_map<std::string, double> model_height = { {
-    "piston_rod_part_red", 0.0065 },  // modified because it sinks into the surface a bit
+    "piston_rod_part_red", 0.0065 },
     { "piston_rod_part_green", 0.0065 }, { "piston_rod_part_blue", 0.0065 }, {
         "pulley_part_red", 0.07 }, { "pulley_part_green", 0.07 }, {
         "pulley_part_blue", 0.07 }, { "gear_part_red", 0.012 }, {
