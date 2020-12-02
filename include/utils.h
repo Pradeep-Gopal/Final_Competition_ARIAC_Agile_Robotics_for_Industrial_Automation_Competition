62fe49dfa157bf67f38b68a3cf5725fe46398a0a
Govind <govindajithkumar@gmail.com>
2020-12-02 09:09:50 -0500

    DOXYGEN COMPLETED : utils.h

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,5 @@
 #ifndef UTILS_H
 #define UTILS_H
-
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Point.h>
 #include <unordered_map>
@@ -12,35 +11,108 @@
 #include <tf2_ros/transform_listener.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
-typedef struct Shipment shipment;  // forward declarations
+
+/**
+ * Struct for Orders
+ */
 typedef struct Order order;
+
+/**
+ * Struct for Shipments
+ */
+typedef struct Shipment shipment; 
+
+/**
+ * Struct for Products
+ */
 typedef struct Product product;
 
-const double PI = 3.141592;  // TODO correct!
+/**
+ * PI Constant for ease of application
+ */
+const double PI = 3.141592;
 
-// Logical cameras
+
+/**
+ * Maximum number of cameras
+ */
 const int MAX_NUMBER_OF_CAMERAS = 18;
 
-const int MAX_PICKING_ATTEMPTS = 3;  // for pickup
-const double ABOVE_TARGET = 0.2;  // above target z pos when picking/placing part
+/**
+ * Maxumum number of attemps for picking up
+ */
+const int MAX_PICKING_ATTEMPTS = 3;
+
+/**
+ * Offset in Z-axis above the target
+ */
+const double ABOVE_TARGET = 0.2;
+
+/**
+ * Timeout for picking the part by the gantry robot
+ */
 const double PICK_TIMEOUT = 4.0;
+
+/**
+ * Timeout for retreiving the part by the gantry robot
+ */
 const double RETRIEVE_TIMEOUT = 2.0;
 
-const double BELT_SPEED = 0.2;  // m/s
+/**
+ * Conveyor belt speed in metres per second
+ */
+const double BELT_SPEED = 0.2;
 
+/**
+ * Height of the gripper attached to the gantry robot
+ */
 const double GRIPPER_HEIGHT = 0.01;
-const double EPSILON = 0.008;  // for the gripper to firmly touch
 
+/**
+ * A small offset to assure that the gripper has been
+ * attached firmly
+ */
+const double EPSILON = 0.008;
+
+/**
+ * a constant for bin height
+ */
 const double BIN_HEIGHT = 0.724;
+
+/**
+ * a constant for tray height
+ */
 const double TRAY_HEIGHT = 0.755;
+
+/**
+ * a constant for rail height
+ */
 const double RAIL_HEIGHT = 0.95;
 
-const double PLANNING_TIME = 20;  // for move_group
-const int MAX_EXCHANGE_ATTEMPTS = 6;  // Pulley flip
+/**
+ * Planning time declared for the move group
+ */
+const double PLANNING_TIME = 20;  
 
+/**
+ * Maximum number of times the exchanges can be
+ * attempted for flipping the pulley parts
+ */
+const int MAX_EXCHANGE_ATTEMPTS = 6;
+
+/**
+ * Array for the action state, declared externally
+ */
 extern std::string action_state_name[];
+
+/**
+ * Hash map for model height, declared externally
+ */
 extern std::unordered_map<std::string, double> model_height;
 
+/**
+ * @brief      Various possible Statuses of the parts 
+ */
 enum PartStates {
   FREE,
   BOOKED,
@@ -52,6 +124,10 @@ enum PartStates {
   LOST
 };
 
+/**
+ * Struct that contains the waypoints of the gantry
+ * robot, arms and the agvs
+ */
 typedef struct PresetLocation {
   std::vector<double> gantry;
   std::vector<double> left_arm;
@@ -59,6 +135,12 @@ typedef struct PresetLocation {
 } start, bin3, bin16, bin13, shelf5, agv2, agv2_drop, shelf5, waypoint_1,
     waypoint_2, waypoint_3, waypoint_4, pose_change, flip_target, agv2_flip;
 
+/**
+ * Part struct that contains the 
+ * model type, its pose, frame, and information 
+ * to check if it is faulty or not, and 
+ * if it has been picked or not 
+ */
 typedef struct Part {
   std::string type;  // model type
   geometry_msgs::Pose pose;  // model pose (in frame)
@@ -72,10 +154,17 @@ typedef struct Part {
   bool picked;
 } part;
 
+/**
+ * Quaternion struct
+ */
 typedef struct Quaternion {
   double w, x, y, z;
 } Quat;
 
+/**
+ * Main struct containing the order,
+ * shipment and product information
+ */
 typedef struct master_struct {
   std::string type;
   geometry_msgs::Pose place_part_pose;
@@ -85,18 +174,30 @@ typedef struct master_struct {
   bool delivered = false;
 } master_struct;
 
+/**
+ * Pick and place struct that contains the
+ * pose for pick and placing the parts 
+ */
 typedef struct pick_and_place {
   std::string type;
   geometry_msgs::Pose pickup_part_pose;
   geometry_msgs::Pose place_part_pose;
 } pick_and_place;
 
+/**
+ * Position struct that contains the 
+ * gantry position information
+ */
 typedef struct Position {
   std::vector<double> gantry;
   std::vector<double> left;
   std::vector<double> right;
 } position;
 
+/**
+ * Shipment struct that holds the
+ * shipment type, agv ID, order and products
+ */
 typedef struct Shipment {
   std::string shipment_type;
   std::string agv_id;
@@ -104,11 +205,15 @@ typedef struct Shipment {
   order *parent_order;
 } shipment;
 
+/**
+ * Product Struct that contains the pose,
+ * agv ID, tray information and high priority order
+ * information
+ */
 typedef struct Product {
   std::string type;
   geometry_msgs::Pose pose;
-  part p;  // NEW here!
-  // std::string frame_of_origin;
+  part p;  
   geometry_msgs::Pose actual_pose;
   std::string actual_pose_frame;
   std::string agv_id;
@@ -123,11 +228,18 @@ typedef struct Product {
   Product();  // contructor
 } product;
 
+/**
+ * Order struct that contains the order ID, and the
+ * vector of shipments
+ */
 typedef struct Order {
   std::string order_id;
   std::vector<nist_gear::Shipment> shipments;
 } order;
 
+/**
+ * Struct that contains the major stats of the simulation
+ */
 typedef struct Stats {
   double total_time = 0.0;
   double fail_time = 0.0;
